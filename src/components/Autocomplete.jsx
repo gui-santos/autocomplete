f33521b9e166a5b330d4f18cd87828c87d3794e0
Guilherme Santos <guilherme.goncalvessantos@gmail.com>
2019-07-17 22:54:06 +0200

    filtering fruits onChange

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -1,18 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { Wrapper, Label, Input } from './AutocompleteStyles';
 
 function Autocomplete(props) {
   const [suggestions, setSuggestions] = useState(props.suggestions);
+  const [filtered, setFiltered] = useState([]);
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    const newSuggestions = suggestions.filter(
+      suggestion => suggestion.toLowerCase().indexOf(value.toLowerCase()) > -1
+    );
+
+    setFiltered(newSuggestions);
+  }, [value, suggestions]);
+
+  const handleChange = e => {
+    const inputValue = e.target.value;
+    setValue(inputValue);
+  };
 
   return (
     <Wrapper>
       <Label htmlFor="autocomplete">Fruit or legume</Label>
-      <Input id="autocomplete" type="text" name="autocomplete" />
+      <Input
+        id="autocomplete"
+        type="text"
+        name="autocomplete"
+        value={value}
+        onChange={handleChange}
+      />
       <ul>
-        {suggestions.map(fruit => (
-          <li>{fruit}</li>
+        {filtered.map((fruit, idx) => (
+          <li key={idx}>{fruit}</li>
         ))}
       </ul>
     </Wrapper>
