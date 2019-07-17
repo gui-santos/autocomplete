75b6206c59ef35476d2813ae3e66a36eba6aa9d4
Guilherme Santos <guilherme.goncalvessantos@gmail.com>
2019-07-17 22:04:01 +0200

    add basci autocomplete and fruits json

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import data from '../fruits.json';
+
 function Autocomplete() {
+  data.map(fruit => console.log(fruit));
   return <input type="text" name="autocomplete" />;
 }
 
