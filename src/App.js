ee5c809f9743893b44a96883063433a01a9168bc
Guilherme Santos <guilherme.goncalvessantos@gmail.com>
2019-07-17 21:29:47 +0200

    styled page container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
 
 import Autocomplete from './components/Autocomplete';
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 10rem 0 0;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <Container>
       <Autocomplete />
-    </div>
+    </Container>
   );
 }
 
